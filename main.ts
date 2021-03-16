c1dd03d903d6b5025a3e544a6a213a85379f68c8
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-03-15 21:58:13 -0700

    Tickle your micro:pet!

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    strip.show()
+    soundExpression.giggle.playUntilDone()
+    strip.clear()
+    strip.show()
+})
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
